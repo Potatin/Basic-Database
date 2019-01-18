f180ee7b721d3c47c7eaabecc9756a90760f686f
Potatin002 <40426229+Potatin002@users.noreply.github.com>
2019-01-18 18:19:43 +0200

    Add project files.

---
+++ Basic Database/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Basic_Database
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.database1DataSet = new Basic_Database.Database1DataSet();
+            this.listElevi = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // listElevi
+            // 
+            this.listElevi.FormattingEnabled = true;
+            this.listElevi.Location = new System.Drawing.Point(12, 12);
+            this.listElevi.Name = "listElevi";
+            this.listElevi.Size = new System.Drawing.Size(156, 368);
+            this.listElevi.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(266, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(249, 368);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 471);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listElevi);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Lista Elevi";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.ListBox listElevi;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Basic Database/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Basic_Database
+{
+    public partial class Form1 : Form
+    {
+        string connectionString;
+        SqlConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["Basic_Database.Properties.Settings.Database1ConnectionString"].ConnectionString;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'database1DataSet.TabelElevi' table. You can move, or remove it, as needed.
+            //this.tabelEleviTableAdapter.Fill(this.database1DataSet.TabelElevi);
+            PopulateElevi();
+        }
+
+        private void PopulateElevi()
+        {
+            string query = "SELECT * FROM TabelElevi";
+
+            using (connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(query,connection))
+            {
+                DataTable tabelElevi = new DataTable();
+                adapter.Fill(tabelElevi);
+
+                listElevi.DisplayMember = "Nume";
+                listElevi.ValueMember = "Id";
+                listElevi.DataSource = tabelElevi;
+                dataGridView1.DataSource = tabelElevi;
+
+            }
+        }
+    }
+}
